c9218ef04d0184333e138d9a1bb1b572718b6d16
Kirtimaanwani <sushilsw111@gmail.com>
2022-10-29 16:56:34 +0530

    added CREDENTIAL.py in sensor/credentials folder

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,6 +1,7 @@
 # write code that shown in class here for mongodb_connection
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+from sensor.credentials.CREDENTIALS import MONGO_DB_URL
 import certifi
 ca = certifi.where()
 
@@ -9,8 +10,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url  = "mongodb+srv://sensor_kirtimaan:WAni1170@cluster0.1p6mzvm.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                MongoDBClient.client = pymongo.MongoClient(MONGO_DB_URL, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
