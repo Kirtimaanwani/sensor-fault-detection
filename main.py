5843972e3bf4f3fc49f57b0ab0b526f4f39b3120
Kirtimaanwani <sushilsw111@gmail.com>
2022-10-30 21:17:25 +0530

    tested Data_Ingestion_Config working fine

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
-
+from sensor.exception import SensorException
+import sys, os  
+from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig 
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print("collection name: " , mongodb_client.database.list_collection_names())
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
