18386ff13741676d6048607d6e9f9ca40539f7a0
Kirtimaanwani <sushilsw111@gmail.com>
2022-10-30 19:36:19 +0530

    changed mongodb secrets as per credential

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -4,13 +4,14 @@ from sensor.constant.database import DATABASE_NAME
 import certifi
 ca = certifi.where()
 
+from credential.CREDENTIAL import MONGO_DB_URL
+
 class MongoDBClient:
     client = None
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url  = "mongodb+srv://sensor_kirtimaan:WAni1170@cluster0.1p6mzvm.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                MongoDBClient.client = pymongo.MongoClient(MONGO_DB_URL, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
