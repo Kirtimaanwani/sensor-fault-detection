66b5bfcbce3f5c07f8bce5d5c17071471f51bf72
Kirtimaanwani <sushilsw111@gmail.com>
2022-10-30 23:49:13 +0530

    raw training pipeline tested ok

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 import sys, os  
 from sensor.logger import logging
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig 
+
+from sensor.pipeline.training_pipeline import TrainPipeline
+
 
 if __name__ == '__main__':
-    training_pipeline_config = TrainingPipelineConfig()
-    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-    print(data_ingestion_config.__dict__)
+    train_pipeline = TrainPipeline()
+    train_pipeline.run_pipeline()
