cc77732fb31c186e7886ce1f19f4e332161ae9f7
Kirtimaanwani <sushilsw111@gmail.com>
2022-10-30 23:48:45 +0530

    raw training pipeline created

---
+++ sensor/pipeline/training_pipeline.py
@@ -0,0 +1,65 @@
+import sys
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact
+
+class TrainPipeline:
+
+    def __init__(self):
+        training_pipeline_config = TrainingPipelineConfig()
+        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        self.training_pipeline_config = training_pipeline_config
+
+
+    def start_data_ingestion(self)->DataIngestionArtifact:
+        try:
+            logging.info("Starting data ingestion")
+            logging.info("Data ingestion complete")
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    
+    def start_data_validation(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    
+    def start_data_transformation(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e, sys)
+
+
+    def start_model_trainer(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e, sys)    
+
+
+    def start_model_evaluation(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    
+    def start_model_pusher(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e, sys)
+
+
+    def run_pipeline(self):
+        try:
+            data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
+        except Exception as e:
+            raise SensorException(e, sys)
+    
+
+    
