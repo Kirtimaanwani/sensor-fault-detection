065b7a1f2220067eaf5e1330a5a5f39cd17c5c50
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-03 00:04:03 +0530

    updates

--- sensor/logger.py
+++ sensor/logger.py
@@ -14,7 +14,7 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format= "[ %(asctime)s ] - %(lineno)d - %(name)s - %(levelname)s - %(message)s",
+    format= "[ %(asctime)s ] - [%(filename)s] - %(funcName)s -  %(lineno)d - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 
 )
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
-HYPHEN_E_DOT = "-e ."
+HYPHEN_E_DOT = '-e .'
 DESCRIPTIONS = "SENSOR FAULT DETECTION"
 
 
@@ -18,7 +18,7 @@ def get_requirements():
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        requirement_list = [str(requirement_name.replace("\n", "")) for requirement_name in requirement_list]
         if HYPHEN_E_DOT in requirement_list:
             requirement_list.remove(HYPHEN_E_DOT)
         return requirement_list
