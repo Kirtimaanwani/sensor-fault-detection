103afab2698474b0e0830771b831766c39ef2082
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-05 19:23:09 +0530

    model trainer constrant, config, artifacts are defined

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -44,3 +44,11 @@ DATA_TRANS_DIR_NAME:str = "data_transformation"
 DATA_TRANS_TRANSFORMED_DATA_DIR:str = "transformed"
 DATA_TRANS_TRANSFORMED_OBJECT_DIR:str = "transformed_object"
 
+"""
+Model Trainer related constants starts with MODEL_var name
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME : str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float =  0.6
