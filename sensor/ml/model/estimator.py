d50b59b3605f3e25e8600b0977410f7888a99429
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-05 21:11:54 +0530

    Added a code to train model and check the accuracy {SensorModel}.

--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -1,4 +1,6 @@
 
+from sensor.exception import SensorException
+import os, sys
 
 class TargetValueMapping:
     def __init__(self):
@@ -11,3 +13,25 @@ class TargetValueMapping:
     def reverse_mapping(self):
         mapping_response = self.to_dict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
+
+
+#A code to train model and check the accuracy.
+
+class SensorModel:
+
+    def __init__(self, preprocessor, model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise SensorException(e, sys)
+    
+    def predict(self,x):
+        try:
+            #with this predict function , directly do preprocessing and prediction-----in future if wanna to add more steps of feature engineering , it can be added here
+           
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise SensorException(e, sys)
