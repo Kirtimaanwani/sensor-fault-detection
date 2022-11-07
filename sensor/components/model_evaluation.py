82a7057569a0fca3be877c65b4057441f9823b42
Kirtimaanwani <sushilsw111@gmail.com>
2022-11-07 13:16:31 +0530

    created model evaluation component , IN PROGRESS TO COMPLETE

---
+++ sensor/components/model_evaluation.py
@@ -0,0 +1,35 @@
+
+
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.entity.artifact_entity import DataValidationArtifact,ModelTrainerArtifact,ModelEvaluationArtifact
+from sensor.entity.config_entity import ModelEvaluationConfig
+import os,sys
+from sensor.ml.metric.classification_metric import get_classification_score
+from sensor.ml.model.estimator import SensorModel
+from sensor.utils.main_utils import save_object,load_object,write_yaml_file
+from sensor.ml.model.estimator import ModelResolver
+from sensor.constant.training_pipeline import TARGET_COLUMN
+from sensor.ml.model.estimator import TargetValueMapping
+import pandas  as  pd
+
+
+class ModelEvaluation:
+
+
+    def __init__(self,model_eval_config:ModelEvaluationConfig,
+                    data_validation_artifact:DataValidationArtifact,
+                    model_trainer_artifact:ModelTrainerArtifact):
+        
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys)
+        
+        
+    
+    def initiate_model_eveluation(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys)
